a68118c84cb640fed6bf3e9c43b3fc83264623d5
Tejas Patil <tejasp094@gmail.com>
2024-05-14 22:28:59 +0530

    admin user added for access to delete chat

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -2,8 +2,18 @@ import { Chat, User } from "../models/chat.model.js";
 
 async function getChatHistory(req, res) {
   try {
-    const chatHistory = await Chat.find({}, "-_id -__v");
-    res.status(200).json(chatHistory);
+    const userId = req.params.id;
+    const adminId = process.env.ADMINID;
+    const chatHistory = await Chat.find({}, "-__v");
+    if (adminId === userId) {
+      const modifiedChatHistory = chatHistory.map((chat) => ({
+        ...chat.toObject(),
+        isAdmin: true,
+      }));
+      res.status(200).json(modifiedChatHistory);
+    } else {
+      res.status(200).json(chatHistory);
+    }
   } catch (error) {
     res.status(400).json(error);
     console.log(error);
@@ -29,12 +39,18 @@ async function saveChatHistory(userName, userId, userMessage, time) {
 
 async function getUserId(req, res) {
   try {
+    const admin = process.env.ADMIN;
     const userName = req.params.username;
-    const user = new User({
-      userName: userName,
-    });
-    const savedUser = await user.save();
-    res.status(200).json(savedUser._id);
+    if (userName === admin) {
+      const adminId = process.env.ADMINID;
+      res.status(200).json(adminId);
+    } else {
+      const user = new User({
+        userName: userName,
+      });
+      const savedUser = await user.save();
+      res.status(200).json(savedUser._id);
+    }
   } catch (error) {
     res.status(400).json(error);
   }
@@ -71,4 +87,31 @@ async function updateUserName(req, res) {
   }
 }
 
-export { saveChatHistory, getUserId, getChatHistory, updateUserName };
+async function deleteChat(req, res) {
+  try {
+    const chatId = req.params.chatId;
+    const userId = req.params.userId;
+    const adminId = process.env.ADMINID;
+    if (userId === adminId) {
+      await Chat.deleteOne({ _id: chatId }).then((result) => {
+        if (result) {
+          res.status(200).json({ message: "Chat deleted successfully" });
+        } else {
+          res.status(500).json({ message: "Error while deleting chat" });
+        }
+      });
+    } else {
+      res.status(404).json({ message: "invalid url" });
+    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
+
+export {
+  saveChatHistory,
+  getUserId,
+  getChatHistory,
+  updateUserName,
+  deleteChat,
+};
--- routes/chat.route.js
+++ routes/chat.route.js
@@ -1,9 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { getUserId, getChatHistory,updateUserName } from "../controllers/chat.controller.js";
+import {
+  getUserId,
+  getChatHistory,
+  updateUserName,
+  deleteChat,
+} from "../controllers/chat.controller.js";
 
 router.get("/username/:username", getUserId);
-router.get("/getChatHistory", getChatHistory);
+router.get("/getChatHistory/userId/:id", getChatHistory);
 router.put("/updateUserName/:id", updateUserName);
+router.delete("/deleteChat/chatId/:chatId/userId/:userId", deleteChat);
 
 export default router;
