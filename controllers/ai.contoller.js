a29d7055e4e7b43c1329f8efe616101cfea555a7
Tejas Patil <tejasp094@gmail.com>
2024-07-31 00:02:22 +0530

    fix chat state management issue

--- controllers/ai.contoller.js
+++ controllers/ai.contoller.js
@@ -3,9 +3,13 @@ import { googleGenerativeAI } from "../connection/ai.connection.js";
 async function getAiResponse(req, res) {
   try {
     const prompt = req.body.prompt;
+    const history = req.body.history;
     const generativeAIModel = new googleGenerativeAI();
     const model = generativeAIModel.generativeAIConnection();
-    const result = await model.generateContentStream(prompt);
+    const chat = model.startChat({
+      history: history,
+    });
+    const result = await chat.sendMessage(prompt);
     const response = await result.response;
     const text = response.text();
     res.status(200).json(text);
